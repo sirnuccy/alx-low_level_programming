152ce3aabe91eaba2d451fe595318bdf7ddae084
Ahmed Sanusi <sanuz.meduz@gmail.com>
2022-08-21 12:47:06 -0700

    Commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,18 +14,3 @@ _putchar(s[index]);
 
 _putchar('\n');
 }
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns length;
- */
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
-}
